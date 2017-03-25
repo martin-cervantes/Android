8264d26cae8c4c1f19816963f0628ed2674b7fcd
mcervantes71 <cervantes.martine@gmail.com>
2017-03-25 12:11:45 -0600

    button

--- app/src/main/java/com/example/mcervantes/coffeeapp/MainActivity.java
+++ app/src/main/java/com/example/mcervantes/coffeeapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mcervantes.coffeeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,19 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view)
+    {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number)
+    {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity);
+        quantityTextView.setText("" + number);
     }
 }
