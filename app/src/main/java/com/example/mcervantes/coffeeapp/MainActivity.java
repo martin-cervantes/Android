eb0e0f74e9e3cfcea8ec2e2b40e728383565b76f
mcervantes71 <cervantes.martine@gmail.com>
2017-03-25 12:36:03 -0600

    price textview price method

--- app/src/main/java/com/example/mcervantes/coffeeapp/MainActivity.java
+++ app/src/main/java/com/example/mcervantes/coffeeapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.mcervantes.coffeeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -15,9 +18,13 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * This method is called when the order button is clicked.
+     */
     public void submitOrder(View view)
     {
         display(1);
+        displayPrice(5);
     }
 
     /**
@@ -27,5 +34,13 @@ public class MainActivity extends AppCompatActivity
     {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
