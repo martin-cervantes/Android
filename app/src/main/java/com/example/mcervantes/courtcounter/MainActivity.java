ed6b144b92269fbd3294fec9c0865b6c2ef61110
Martin Cervantes <cervantes.martine@gmail.com>
2017-03-30 13:13:46 -0600

    Update MainActivity.java
    add to score for team A methods

--- app/src/main/java/com/example/mcervantes/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/mcervantes/courtcounter/MainActivity.java
@@ -3,11 +3,39 @@ package com.example.mcervantes.courtcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    int scoreTeamA = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //dispalyForTeamA(8);
+    }
+    
+    public void displayForTeamA(int score)
+    {
+        TextView scoreView = (TextView) findViewById(R.id.counterA);
+        scoreView.setText(String.valueOf(score));
+    }
+    
+    public void addThreeForTeamA(View view)
+    {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+    
+    public void addTwoForTeamA(View view)
+    {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+    
+    public void addOneForTeamA(View view)
+    {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
     }
 }
