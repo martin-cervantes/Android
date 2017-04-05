5671a5b01fbf9a86bff658065ba29d83469e8161
mcervantes71 <cervantes.martine@gmail.com>
2017-04-05 12:46:38 -0600

    email intent

--- app/src/main/java/com/example/mcervantes/coffeeapp/MainActivity.java
+++ app/src/main/java/com/example/mcervantes/coffeeapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.mcervantes.coffeeapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +9,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity
     int quantity = 0;
     boolean hasWhippedCream = false;
     boolean hasChololate = false;
+    String name = "Martin Cervantes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,34 +36,64 @@ public class MainActivity extends AppCompatActivity
         hasChololate = ChololateCheckBox.isChecked();
 
         EditText nameEditText = (EditText) findViewById(R.id.name);
-        String name = nameEditText.getText().toString();
+        String Name = nameEditText.getText().toString();
+
+        if (!Name.equals(""))
+        {
+            name = nameEditText.getText().toString();
+        }
 
         TextView orderSummaryTextView = (TextView) findViewById(R.id.orderSummary);
-        orderSummaryTextView.setText(createOrderSummary(calculatePrice(), name));
+        //orderSummaryTextView.setText(createOrderSummary(calculatePrice(), name));
+
+        createOrderSummary(calculatePrice(), name);
+
     }
 
-    public String createOrderSummary(int price, String customer)
+    public void createOrderSummary(int price, String customer)
     {
-        return "Name = "+ customer +"\n" +
+        String message =  "Name = "+ customer +"\n" +
                 "Add whipped cream? "+ hasWhippedCream +"\n" +
                 "Add chocolate? "+ hasChololate +"\n" +
                 "Quantity = " + quantity + "\n" +
                 "Total = " + NumberFormat.getCurrencyInstance().format(price) + "\n" +
                 "Thank you!! :D";
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, "cervantes.martine@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee App");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
+
     }
 
     public void increment(View view)
     {
-        quantity = quantity + 1;
+        if(quantity < 50)
+        {
+            quantity = quantity + 1;
+        }
+        else
+        {
+            Toast.makeText(this, "You cannot have more than 50 coffees :(", Toast.LENGTH_LONG).show();
+        }
 
         displayQuantity();
     }
 
     public void decrement(View view)
     {
-        if(quantity > 0)
+        if(quantity > 1)
         {
             quantity = quantity - 1;
+        }
+        else
+        {
+            Toast.makeText(this, "You cannot have less than 1 coffee :(", Toast.LENGTH_LONG).show();
         }
 
         displayQuantity();
