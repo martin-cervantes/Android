ae4d4777a7130d12645c54a43a501a9c85bef52f
mcervantes71 <cervantes.martine@gmail.com>
2017-04-19 10:31:11 -0600

    Explicit intent
    use an intent to open another activity

--- app/src/main/java/com/example/mcervantes/miwok/MainActivity.java
+++ app/src/main/java/com/example/mcervantes/miwok/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mcervantes.miwok;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +15,29 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList (View view)
+    {
+        Intent i = new Intent(this, Numbers.class);
+        startActivity(i);
+    }
+
+    public void openFamilyMembersList (View view)
+    {
+        Intent i = new Intent(this, FamilyMembers.class);
+        startActivity(i);
+    }
+
+    public void openColorsList (View view)
+    {
+        Intent i = new Intent(this, Colors.class);
+        startActivity(i);
+    }
+
+    public void openPhrasesList (View view)
+    {
+        Intent i = new Intent(this, Phrases.class);
+        startActivity(i);
     }
 }
