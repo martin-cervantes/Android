4469bca32608ca21fcf0d3e17d73faa387bef8f0
mcervantes71 <cervantes.martine@gmail.com>
2017-04-22 07:47:27 -0600

    GridView + ArrayAdapter

--- app/src/main/java/com/example/mcervantes/miwok/Numbers.java
+++ app/src/main/java/com/example/mcervantes/miwok/Numbers.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
@@ -43,13 +44,13 @@ public class Numbers extends AppCompatActivity
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // activity_numbers.xml layout file.
-        ListView listView = (ListView) findViewById(R.id.activity_numbers);
+        GridView gridView = (GridView) findViewById(R.id.activity_numbers);
 
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
         // {@link ListView} will display list items for each word in the list of words.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in
         // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        listView.setAdapter(itemsAdapter);
+        gridView.setAdapter(itemsAdapter);
 
     }
 }
