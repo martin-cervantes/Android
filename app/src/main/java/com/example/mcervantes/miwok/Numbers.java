deeeb4cbe1e64e83663da49b831721c4d77d8c7f
mcervantes71 <cervantes.martine@gmail.com>
2017-05-03 20:27:48 -0600

    setOn   Item   ClickListener

--- app/src/main/java/com/example/mcervantes/miwok/Numbers.java
+++ app/src/main/java/com/example/mcervantes/miwok/Numbers.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
+
 
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -49,12 +49,12 @@ public class Numbers extends AppCompatActivity
 
         listView.setAdapter(adapter);
 
-        listView.setOnClickListener(new AdapterView.OnItemClickListener()
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
-                Toast.makeText(Numbers.this, "List Item clicked", Toast.LENGTH_SHORT);
+                Toast.makeText(Numbers.this, "List item clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
