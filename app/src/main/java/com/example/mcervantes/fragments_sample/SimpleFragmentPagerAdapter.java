55ec4e62b4b3bc09a0e1bfad81984d0f2f470537
mcervantes71 <cervantes.martine@gmail.com>
2017-05-09 06:38:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mcervantes/fragments_sample/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.mcervantes.fragments_sample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter
+{
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm)
+    {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        if (position == 0)
+        {
+            return new MondayFragment();
+        }
+        else if (position == 1)
+        {
+            return new TuesdayFragment();
+        }
+        else if (position == 2)
+        {
+            return new WednesdayFragment();
+        }
+        else if (position == 3)
+        {
+            return new ThursdayFragment();
+        }
+        else
+        {
+            return new FridayFragment();
+        }
+    }
+
+    @Override
+    public int getCount()
+    {
+        return 5;
+    }
+}
