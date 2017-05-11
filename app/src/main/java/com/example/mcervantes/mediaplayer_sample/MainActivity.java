2dc0acebd3bcdf4e02a443dca290378c07e1684e
mcervantes71 <cervantes.martine@gmail.com>
2017-05-11 07:41:28 -0600

    music player

--- app/src/main/java/com/example/mcervantes/mediaplayer_sample/MainActivity.java
+++ app/src/main/java/com/example/mcervantes/mediaplayer_sample/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.mcervantes.mediaplayer_sample;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private MediaPlayer mediaPlayer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.comedia);
+
+        Button playButton = (Button)findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mediaPlayer.start();
+
+                Toast.makeText(MainActivity.this,"Play",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button pauseButton = (Button)findViewById(R.id.play_button);
+
+        pauseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mediaPlayer.start();
+
+                Toast.makeText(MainActivity.this,"Play",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
