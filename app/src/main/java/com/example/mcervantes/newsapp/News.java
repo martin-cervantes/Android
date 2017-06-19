950eca7f1518307464a95283ea0abb9bad5b01e0
Martin Cervantes <cervantes.martine@gmail.com>
2017-06-18 23:23:02 -0600

    Update News.java

--- app/src/main/java/com/example/mcervantes/newsapp/News.java
+++ app/src/main/java/com/example/mcervantes/newsapp/News.java
@@ -4,5 +4,26 @@ package com.example.mcervantes.newsapp;
  * Created by mcervantes on 6/16/17.
  */
 
-public class News {
+public class News
+{
+  private String content;
+  
+  private String tag;
+  
+  private String date;
+  
+  public String getContent()
+  {
+    return content;
+  }
+  
+  public String getTag()
+  {
+    return tag;
+  }
+  
+  public String getDate()
+  {
+    return date;
+  }
 }
