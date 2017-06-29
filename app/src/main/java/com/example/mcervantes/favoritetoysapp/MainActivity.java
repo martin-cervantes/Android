bbeee6f00a9ddbb3665fe87e3d5adc5122443144
Martin Cervantes <cervantes.martine@gmail.com>
2017-06-29 16:50:46 -0600

    scroll view

--- app/src/main/java/com/example/mcervantes/favoritetoysapp/MainActivity.java
+++ app/src/main/java/com/example/mcervantes/favoritetoysapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.mcervantes.favoritetoysapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView mToysListTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+
+        String[] toyNames = ToyBox.getToyNames();
+
+        for (String toyName : toyNames)
+        {
+            mToysListTextView.append(toyName + "\n\n\n");
+        }
     }
 }
