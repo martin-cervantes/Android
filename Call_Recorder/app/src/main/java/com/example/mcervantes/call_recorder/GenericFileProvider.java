cdec986719e05ab244ecf48ccf16b1f520ef54d2
Martin Cervantes <cervantes.martine@gmail.com>
2018-10-15 18:58:02 -0600

    call_recorder

---
+++ Call_Recorder/app/src/main/java/com/example/mcervantes/call_recorder/GenericFileProvider.java
@@ -0,0 +1,14 @@
+package com.example.mcervantes.call_recorder;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by VS00481543 on 14-12-2017.
+ */
+
+public class GenericFileProvider extends FileProvider {
+
+
+    public GenericFileProvider() {
+    }
+}
