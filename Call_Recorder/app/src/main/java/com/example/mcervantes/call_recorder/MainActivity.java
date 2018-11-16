4be30d6b65d8ea4bbb75361a3f87a8374c1e21c6
Martin Cervantes <cervantes.martine@gmail.com>
2018-11-16 07:53:49 -0700

    call recorder

--- Call_Recorder/app/src/main/java/com/example/mcervantes/call_recorder/MainActivity.java
+++ Call_Recorder/app/src/main/java/com/example/mcervantes/call_recorder/MainActivity.java
@@ -1,43 +1,13 @@
 package com.example.mcervantes.call_recorder;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Switch;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
-    Switch startAndStop;
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        startAndStop = (Switch) findViewById(R.id.toggleButton);
-
-    }
-
-    public void toogleButton(View view)
-    {
-        boolean checked = ((Switch)view).isChecked();
-
-        if(checked)
-        {
-            Intent intent = new Intent(this,RecordingService.class);
-            startService(intent);
-
-            Toast.makeText(getApplicationContext(), "Call Recording STARTED", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            Intent intent = new Intent(this,RecordingService.class);
-            stopService(intent);
-
-            Toast.makeText(getApplicationContext(), "Call Recording STOPED", Toast.LENGTH_SHORT).show();
-        }
     }
 }
