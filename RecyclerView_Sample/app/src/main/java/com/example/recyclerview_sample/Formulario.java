e44c5055c00856ae2f35082c8a45980af013b887
Martin Cervantes <cervantes.martine@gmail.com>
2020-02-05 11:56:40 -0700

    RecyclerView_sample

---
+++ RecyclerView_Sample/app/src/main/java/com/example/recyclerview_sample/Formulario.java
@@ -0,0 +1,15 @@
+package com.example.recyclerview_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formulario extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.formulario);
+    }
+}
